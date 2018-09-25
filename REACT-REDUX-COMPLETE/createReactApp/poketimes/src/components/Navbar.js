b6dd6d5ca4468eca70bbd40249ba48c1bceecaa8
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-09-25 16:58:07 -0400

    React router implemented

--- REACT-REDUX-COMPLETE/createReactApp/poketimes/src/components/Navbar.js
+++ REACT-REDUX-COMPLETE/createReactApp/poketimes/src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
             <div className="container">
                 <a className="brand logo">Poke'Times</a>
                 <ul className="right">
-                    <li> <a href="/home">Home</a></li>
+                    <li> <a href="/">Home</a></li>
                     <li> <a href="/about">About</a></li>
                     <li> <a href="/contact">Contact</a></li>
                 </ul>
