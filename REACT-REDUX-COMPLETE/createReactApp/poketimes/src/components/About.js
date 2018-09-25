2eb10a856a2a94bcdd696fb839829b2121f440f7
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-09-25 12:35:46 -0400

    Nar bar displayed

---
+++ REACT-REDUX-COMPLETE/createReactApp/poketimes/src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero et, nihil sapiente,
+             enim inventore sequi perferendis dolorum, modi eveniet voluptate repudiandae reprehenderit!
+              Beatae, aut? Placeat quia voluptates autem nam numquam.
+            </p>
+
+        </div>
+    )
+}
+
+export default About
---
+++ REACT-REDUX-COMPLETE/createReactApp/poketimes/src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero et, nihil sapiente,
+             enim inventore sequi perferendis dolorum, modi eveniet voluptate repudiandae reprehenderit!
+              Beatae, aut? Placeat quia voluptates autem nam numquam.
+            </p>
+
+        </div>
+    )
+}
+
+export default Contact
